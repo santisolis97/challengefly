6ab0441bd648634cc09a6c951a6a0bbace1b03b5
Santiago <santux09@gmail.com>
2021-03-26 16:44:18 -0300

    search ready, need to show only

--- src/App.js
+++ src/App.js
@@ -1,13 +1,87 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Header from "./components/Header";
 import Movies from "./components/Movies";
+import axios from "axios";
+
 function App() {
+  const [searchInput, setSearchInput] = useState("");
+  const [search, setSearch] = useState(false);
+  const [defResults, setDefResults] = useState({});
+  const [searchResults, setSearchResults] = useState({});
+  const [loading, setLoading] = useState(true);
+  const apiKey = "eeec2429b31a143e915ed959aad485f0";
+  const handleSearchInputChange = (event) => {
+    setSearchInput(event.target.value);
+  };
+  const handleSearch = (event) => {
+    event.preventDefault();
+    setSearch(true);
+  };
+  useEffect(() => {
+    const fetchMovies = () => {
+      if (search) {
+        fetchSearch();
+      } else {
+        fetchTopRatedMovies();
+      }
+    };
+    const fetchSearch = () => {
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${searchInput}&page=1&include_adult=false`;
+      axios
+        .get(url)
+        .then(function (res) {
+          console.log("se realizo fetch de busqueda");
+          console.log(res.data);
+          //   setSearchResults(res.data);
+          setSearchResults(res.data);
+          setSearch(false);
+          setLoading(false);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    };
+    const fetchTopRatedMovies = () => {
+      const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`;
+      axios.get(url).then((res) => {
+        // setTopRatedMovies(res.data);
+        setDefResults(res.data);
+
+        console.log("Se realizo fetch completo");
+        setLoading(false);
+      });
+    };
+
+    fetchMovies();
+    // fetchTopRatedMovies();
+  }, [search]);
   return (
     <div className="App">
       <Header />
       <div className="container">
-        <Movies></Movies>
+        <div className="Form">
+          <div className="d-flex justify-content-center ">
+            <form className="form-inline" onSubmit={handleSearch}>
+              <input
+                className="form-control mr-sm-2"
+                type="search"
+                value={searchInput}
+                onChange={handleSearchInputChange}
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button
+                className="btn btn-outline-success my-2 my-sm-0"
+                type="submit"
+              >
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+        <Movies results={defResults} loading={loading}></Movies>
       </div>
     </div>
   );
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,57 +1,40 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect } from "react";
 import Movie from "./Movie";
-function Movies() {
-  const [topRatedMovies, setTopRatedMovies] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [searchInput, setSearchInput] = useState("");
-  const apiKey = "eeec2429b31a143e915ed959aad485f0";
-  const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`;
-
-  const handleSearchInputChange = (event) => {
-    setSearchInput(event.target.value);
-    console.log(searchInput);
-  };
-  const handleSearch = (event) => {};
-  const fetchTopRatedMovies = () => {
-    axios.get(url).then((res) => {
-      setTopRatedMovies(res.data);
-      console.log(res.data);
-      setLoading(false);
-    });
-  };
-  useEffect(() => {
-    fetchTopRatedMovies();
-  }, []);
+function Movies(props) {
   return (
     <div>
-      <div className="d-flex justify-content-center ">
-        <form className="form-inline" onSubmit={handleSearch}>
-          <input
-            className="form-control mr-sm-2"
-            type="search"
-            value={searchInput}
-            onChange={handleSearchInputChange}
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button
-            className="btn btn-outline-success my-2 my-sm-0"
-            type="submit"
-          >
-            Search
-          </button>
-        </form>
-      </div>
-
-      {loading && (
+      {props.loading && (
         <div className="text-center">
           <div className="spinner-border" role="status">
             <span className="visually-hidden">Loading...</span>
           </div>
         </div>
       )}
+      {!props.loading && (
+        <div>
+          <h3>Top rated movies:</h3>
+          <div className="row">
+            {props.results.results.map((item, index) => (
+              <div key={index} className="col-3">
+                <Movie result={item} />
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
 
+      {/* {searchResults.length > 0 && !loading && (
+        <div>
+          <h3>Top rated movies:</h3>
+          <div className="row">
+            {searchResults.map((item, index) => (
+              <div key={index} className="col-3">
+                {item.title}
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
       {!loading && (
         <div>
           <h3>Top rated movies:</h3>
@@ -63,7 +46,7 @@ function Movies() {
             ))}
           </div>
         </div>
-      )}
+      )} */}
     </div>
   );
 }
